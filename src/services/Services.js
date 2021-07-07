6ae3072b6778cfa007731c0582e52d2d88669cf8
ThiagoJA <thiago_jesuino@hotmail.com>
2021-07-07 01:55:18 -0300

    Criar pagina de login

---
+++ src/services/Services.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const httpClient = axios.create({
+  baseURL: 'https://projeto-integrador-4.herokuapp.com',
+  headers: {
+    'Content-type': 'application/json',
+  },
+});
+
+export const login = (data) => httpClient.post('/auth/login', data);
+
+export const forgot = (data) => httpClient.post('/auth/forgot', data);
+
+export const create = (data) => httpClient.post('/livro', data);
+
+export const update = (id, data) => httpClient.put(`/livro/${id}`, data);
+
+export const remove = (id) => httpClient.delete(`/livro/${id}`);
